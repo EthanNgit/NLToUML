1b2b3c56b8118317d3911e023c6f8ce2820535b0
Ethan North <northethana@gmail.com>
2025-04-12 16:53:47 -0500

    initial setup + frontend mock

---
+++ backend/src/main/kotlin/com/group15/nltouml/NltoumlApplication.kt
@@ -0,0 +1,11 @@
+package com.group15.nltouml
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NltoumlApplication
+
+fun main(args: Array<String>) {
+	runApplication<NltoumlApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/group15/nltouml/NltoumlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.group15.nltouml
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NltoumlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
