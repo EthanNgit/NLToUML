ab1770d52f1112ac7b56c8efc983e77dcb33f230
Ethan North <northethana@gmail.com>
2025-04-26 14:38:15 -0500

    modularization + not hardcoding llm specific details

--- backend/src/main/kotlin/com/group15/nltouml/model/AiEngineModel.kt
+++
@@ -1,5 +0,0 @@
-package com.group15.nltouml.model
-
-enum class AiEngineModel {
-    Local, OpenAI, DeepSeek, Gemini
-}
--- backend/src/main/kotlin/com/group15/nltouml/model/MethodBody.kt
+++
@@ -1,6 +0,0 @@
-package com.group15.nltouml.model
-
-data class MethodBody (
-    val icon: ByteArray,
-    val name: String
-)
